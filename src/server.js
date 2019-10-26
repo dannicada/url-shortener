371dec3ef8a7708437e59d7e2701511ba3039a12
dannicada <dannynwangwu@gmail.com>
2019-10-26 14:58:53 +0100

    added nanoid to generate secure random short id

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const dns = require('dns');
 const {MongoClient} = require('mongodb');
+const nanoid = require('nanoid');
 
 const databaseUrl = process.env.DATABASE
 
@@ -15,16 +16,38 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-MongoClient.connect(databaseUrl, { useNewUrlParser: true})
- .then(client => {
-     app.locals.db = client.db('shortener');
- })
- .catch(() => console.error('Failed to connect to the database'));
+
+
+MongoClient.connect(databaseUrl, { useNewUrlParser: true })
+  .then(client => {
+    app.locals.db = client.db('shortener');
+  })
+  .catch(() => console.error('Failed to connect to the database'));
+
+const shortenURL = (db, url) => {
+  const shortenedURLs = db.collection('shortenedURLs');
+  return shortenedURLs.findOneAndUpdate({ original_url: url },
+    {
+      $setOnInsert: {
+        original_url: url,
+        short_id: nanoid(7),
+      },
+    },
+    {
+      returnOriginal: false,
+      upsert: true,
+    }
+  );
+};
+
+
 
 app.get('/', (req, res) => {
     const htmlPath = path.join(__dirname, 'public', 'index.html');
     res.sendFile(htmlPath);
 });
+
+
 
 app.post('/new', (req, res) => {
     let originalUrl;
@@ -39,6 +62,16 @@ app.post('/new', (req, res) => {
             return res.status(404).send({ error: 'Address not found'});
         };
         console.log(req.body);
+        const {db} = req.app.locals;
+        shortenURL (db, originalUrl.href)
+            .then(result => {
+                const doc = result.value;
+                res.json({
+                    original_url: doc.original_url,
+                    short_id: doc.short_id,
+                });
+            })
+            .catch(console.error);
 
     });
 });
